fc4dbb440318d195199c0cec94c755372966d87c
alexislee <aleelifeissimple4848@gmail.com>
2025-11-03 19:56:06 -0800

    Started writing dataset organization script

---
+++ Prepare_Dataset.py
@@ -0,0 +1,8 @@
+import os
+
+#Make sub folders with labels of the letters and numbers within the train, test, and validation
+def create_sub_folders(labels: list , data_directory: str, subset:str="train"):
+    pass
+
+def get_labels(data_directory:str)->list:
+    pass
